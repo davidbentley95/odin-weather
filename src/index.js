ef4dfd8a9719808399be7a98729922117d75480a
David Bentley <david.bentley95@gmail.com>
2025-03-29 07:20:34 -0700

    Add formating and adjust DOM elements

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ let unitGroup = "uk";
 async function getWeatherData() {
 
     let apiSearchQuery = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityQuery}?unitGroup=${unitGroup}&key=Q5PNNCTCNG9SWP7EEZQX9CPKG&contentType=json`;
-    
+
     const response = await fetch(apiSearchQuery, {mode: 'cors'});
     const data = await response.json();
     const location = data.resolvedAddress;
@@ -36,34 +36,56 @@ function updatePageDate(location, icon, temp, sunrise, sunset, humidity, uvindex
     iconElement.classList.add("weather-icon-text");
     iconElement.textContent = icon;
 
+    let tempUnit;
+
+    if(unitGroup === "uk") {
+        tempUnit = "°C"
+    } else {
+        tempUnit = "°F"
+    }
+
     const tempElement = document.createElement("p");
     tempElement.classList.add("temperature");
-    tempElement.textContent = `Temperature: ${temp}`;
+    tempElement.textContent = `${temp}${tempUnit}`;
 
     const sunriseElement = document.createElement("p");
     sunriseElement.classList.add("sunrise");
-    sunriseElement.textContent = `Sunrise: ${sunrise}`;
+    sunriseElement.textContent = "Sunrise:";
+    const sunriseSpan = document.createElement("span");
+    sunriseSpan.textContent = sunrise; 
+    sunriseElement.append(sunriseSpan);
 
     const sunsetElement = document.createElement("p");
     sunsetElement.classList.add("sunset");
-    sunsetElement.textContent = `Sunset: ${sunset}`;
+    sunsetElement.textContent = "Sunset:";
+    const sunsetSpan = document.createElement("span");
+    sunsetSpan.textContent = sunset; 
+    sunsetElement.append(sunsetSpan);
 
     const humidityElement = document.createElement("p");
     humidityElement.classList.add("humidity");
-    humidityElement.textContent = `Humidity: ${humidity}%`;
+    humidityElement.textContent = "Humidity:";
+    const humiditySpan = document.createElement("span");
+    humiditySpan.textContent = `${humidity}%`; 
+    humidityElement.append(humiditySpan);
 
     const uvIndexElement = document.createElement("p");
     uvIndexElement.classList.add("uv-index");
     uvIndexElement.textContent = `UV Index: ${uvindex}`;
+    const uvIndexSpan = document.createElement("span");
+    uvIndexSpan.textContent = uvindex; 
+    uvIndexElement.append(uvIndexSpan);
+
+    const minorStats = document.createElement("div");
+    minorStats.classList.add("minor-stats-div");
+
+    minorStats.append(sunriseElement, sunsetElement, humidityElement, uvIndexElement)
 
     // Append elements to the container
     container.appendChild(locationElement);
     container.appendChild(iconElement);
     container.appendChild(tempElement);
-    container.appendChild(sunriseElement);
-    container.appendChild(sunsetElement);
-    container.appendChild(humidityElement);
-    container.appendChild(uvIndexElement);
+    container.appendChild(minorStats);
 }
 
 
